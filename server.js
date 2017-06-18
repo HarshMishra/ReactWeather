a634366c98834cc14c6a33b905abeb1dd2dbcbea
Harsh Mishra <harsh.mishra@comptel.com>
2017-06-18 09:56:44 +0530

    Prep code for heroku

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var app = express();
-const PORT = process.env.PORT || 3000 ;
+const PORT = process.env.PORT || 3001 ;
+app.use(function(req, res, next){
+if (req.headers['x-forwarded-proto'] === 'http'){
+next();
+}
+  else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('public'));
-
 app.listen(PORT , function()
 {
   console.log('express server is up on port ' + PORT);

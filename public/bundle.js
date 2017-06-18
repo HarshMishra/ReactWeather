a76ebe5f6d6550c2be17f4c37c9c68bb00fda2c8
Harsh Mishra <harsh.mishra@comptel.com>
2017-06-18 11:29:29 +0530

    Modified About page

--- public/bundle.js
+++ public/bundle.js
@@ -26652,9 +26652,18 @@
 
 	var About = function About(props) {
 	  return React.createElement(
-	    'h3',
+	    'div',
 	    null,
-	    'About Component'
+	    React.createElement(
+	      'h3',
+	      null,
+	      'About Component'
+	    ),
+	    React.createElement(
+	      'p',
+	      null,
+	      'Welcome to About page'
+	    )
 	  );
 	};
 
